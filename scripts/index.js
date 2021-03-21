33a4c1650a2392f7fc5980f30bcae33b7f1b2bfc
Aleksandr Bondarchuk <bondarchukamg@gmail.com>
2021-03-21 18:03:03 +0300

    feat: добавить интерактивный попап на js

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,16 @@
-let popup = document.querySelector('.popup'); //сохранил попап в переменную popup (в переменной лежит объект с разметкой html)
-/*console.log(popup);*/
-let openPopupButton = document.querySelector('.profile__edit-button');//сохраняем  кнопку редактировать в переменную.
-let closePopupButton = document.querySelector('.popup__close-button');//сохраняем  кнопку закрыть в переменную.
-let containerPopupButton = document.querySelector('.popup__container');//сохраняем  кнопку подложка в переменную.
-let formPopupButton = document.querySelector('.popup__form');//сохраняем  форму чтобы при клике на форму поп-ап не исчезал
+let popup = document.querySelector('.popup'); //сохранил попап в переменную popup
+let openPopupButton = document.querySelector('.profile__edit-button');  //сохраняем  кнопку редактировать в переменную.
+let closePopupButton = document.querySelector('.popup__close-button');  //сохраняем  кнопку закрыть в переменную.
+let containerPopupButton = document.querySelector('.popup__container');  //сохраняем  кнопку подложка в переменную.
+let formPopupButton = document.querySelector('.popup__form');  //сохраняем  форму чтобы при клике на форму поп-ап не исчезал
+let formElement = document.querySelector('.popup__form'); // сохраняем форму
+let nameInput = document.querySelector('.popup__input-name');  //Находим поля формы в DOM
+let jobInput = document.querySelector('.popup__input-job');  //Находим поля формы в DOM
+let titleProfile = document.querySelector('.profile__title'); //сохраняем заголовок профиля title
+let textProfile = document.querySelector('.profile__text'); //сохраняем текст профиля
 
 function openPopup() {
-  popup.classList.add('popup_opened'); //поп-ап откарвается (добавляем класс). Почему тут пишем класс без точки popup_opened?
+  popup.classList.add('popup_opened'); //поп-ап откырвается (добавляем класс). Почему тут пишем класс без точки popup_opened, потому что метод!?
 }
 
 function closePopup() {
@@ -15,17 +19,31 @@ function closePopup() {
 
 //добавляем кнопке редактировать слушатель
 openPopupButton.addEventListener('click', function() {
-  closePopup();
+  openPopup();
 });
-
 //добавляем кнопке закрыть слушатель
 closePopupButton.addEventListener('click', function() {
-  openPopup();
+  closePopup();
 });
 //добавляем нажатию на слой контейнер слушатель
 containerPopupButton.addEventListener('click', function() {
-   openPopup();
+  closePopup();
 });
+
 formPopupButton.addEventListener('click', function(event) {
   event.stopImmediatePropagation();
-}); //добавляем стоп чтобы при нажатии на форму поп-ап не закрывался
+}); //добавляем стоп чтобы при нажатии на форму поп-ап не закрывался из Livecoding
+
+
+function formSubmitHandler (evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+    titleProfile.textContent = nameInput.value;  // Выберите элементы, куда должны быть вставлены значения полей
+    textProfile.textContent = jobInput.value; // Вставьте новые значения с помощью textContent
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('submit', formSubmitHandler);
+formElement.addEventListener('submit', closePopup); //закрытие попапа при нажатии сохранить
+
+
